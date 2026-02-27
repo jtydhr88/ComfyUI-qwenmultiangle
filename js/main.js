a0e49741083a69c5b4024a02a56681c523724b8e
高鑫崧 <50285788+SongDI911@users.noreply.github.com>
2026-02-27 11:14:01 +0800

    <feat> The inaccurate display caused by the adaptation path issue

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var __defProp = Object.defineProperty;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
-import { app } from "../../../scripts/app.js";
-import { api } from "../../../scripts/api.js";
+// import { app } from "../../../scripts/app.js";
+// import { api } from "../../../scripts/api.js";
+const { app } = window.comfyAPI.app;
+const { api } = window.comfyAPI.api;
 /**
  * @license
  * Copyright 2010-2024 Three.js Authors
